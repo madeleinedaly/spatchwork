38d59b14b976760b5ad90c38f4e10b2fa49e49cc
Madeleine Daly <mdaly@dailybreak.com>
2018-10-14 14:31:08 -0400

    use meow for iterate script CLI

--- iterate.js
+++ iterate.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 'use strict';
-const chunk = require('lodash.chunk');
-const range = require('lodash.range');
+const meow = require('meow');
+const chunk = require('lodash/chunk');
+const range = require('lodash/range');
 const child_process = require('child_process');
 const path = require('path');
 const util = require('util');
@@ -15,16 +16,31 @@ const [mkdir, readdir, copyFile, execFile] = [
   child_process.execFile
 ].map(util.promisify);
 
-(async ([
-  frames, // how many collages to make
-  input   // base image to collage on
-]) => {
+const options = {
+  description: false,
+  help: `
+Usage:
+  ${path.basename(process.argv[1])} <file> [options]
+Options:
+  -n --num_frames <frames>  How many frames (collages) to generate. [default: 32]
+  -h --help                 Print this message and exit.
+`,
+  flags: {
+    frames: {type: 'number', alias: 'n', default: 32},
+    modulate: {type: 'boolean', alias: 'm', default: false}
+  }
+};
+
+(async cli => {
+  if (cli.flags.h) cli.showHelp(0);
+  if (cli.input.length < 1) cli.showHelp(2);
+
   const pwd = process.cwd();
-  const concurrency = os.cpus().length;
+  const input = cli.input.shift();
   const target = path.resolve(pwd, input);
   const files = await readdir(path.resolve(pwd, 'images'));
   const jpegs = files.filter(file => file.endsWith('.jpeg'));
-  const sizes = range((jpegs.length - frames), jpegs.length);
+  const sizes = range((jpegs.length - cli.flags.frames), jpegs.length);
   const pad = s => String(s).padStart(sizes[sizes.length - 1].length, '0');
 
   // create target dirs and copy base image to them in parallel
@@ -37,11 +53,11 @@ const [mkdir, readdir, copyFile, execFile] = [
   }));
 
   // generate one collage per core
-  for (const subset of chunk(tasks, concurrency)) {
+  for (const subset of chunk(tasks, os.cpus().length)) {
     await Promise.all(subset.map(task => {
       const {size, dir} = task;
-      console.log('%s (%s/%d)', pad(size), pad(tasks.indexOf(task)), sizes.length);
+      console.log(`${pad(size)} (${pad(tasks.indexOf(task) + 1)}/${sizes.length})`);
       return execFile('python', ['segment.py', size, dir]);
     }));
   }
-})(process.argv.slice(2));
+})(meow(options));
